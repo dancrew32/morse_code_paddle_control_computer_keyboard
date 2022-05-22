976f65af9141e6349796691d336eddb6e6f01710
Dan Masquelier <dancrew32@gmail.com>
2022-05-21 22:39:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,115 +3,190 @@ import time
 
 from pynput import mouse, keyboard
 
-# TODO(DAN): find something the plays asynchronously and isn't so laggy
-from pysinewave import SineWave
-
+controller = keyboard.Controller()
 
 MORSE = {
-        'A':'.-',
-        'B':'-...',
-        'C':'-.-.',
-        'D':'-..',
-        'E':'.',
-        'F':'..-.',
-        'G':'--.',
-        'H':'....',
-        'I':'..',
-        'J':'.---',
-        'K':'-.-',
-        'L':'.-..',
-        'M':'--',
-        'N':'-.',
-        'O':'---',
-        'P':'.--.',
-        'Q':'--.-',
-        'R':'.-.',
-        'S':'...',
-        'T':'-',
-        'U':'..-',
-        'V':'...-',
-        'W':'.--',
-        'X':'-..-',
-        'Y':'-.--',
-        'Z':'--..',
-        '1':'.----',
-        '2':'..---',
-        '3':'...--',
-        '4':'....-',
-        '5':'.....',
-        '6':'-....',
-        '7':'--...',
-        '8':'---..',
-        '9':'----.',
-        '0':'-----',
-        ', ':'--..--',
-        '.':'.-.-.-',
-        '?':'..--..',
-        '/':'-..-.',
-        '-':'-....-',
-        '(':'-.--.',
-        ')':'-.--.-',
-        '!':'-.-.--',
-  
-        ## TODO(DAN): come up with common shortcuts for CW prosigns
-        'esc':'........',
-        '=':'-...-',
-        }
+
+    # Letters
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+
+    # Digits
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+
+    # Punctuation
+    ',': '--..--',
+    '.': '.-.-.-',
+    '?': '..--..',
+    '/': '-..-.',
+    '-': '-....-',
+    '(': '-.--.',
+    ')': '-.--.-',
+    '!': '-.-.--',
+    '+': '.-.-.',
+    '-': '-....-',
+    '_': '..--.-',
+    '"': '.-..-.',
+    '$': '...-..-',
+    '@': '.--.-.',
+    '=': '-...-',
+
+    # Getting creative with less useful chars
+    '¿': '..-.-', # Left alt?
+    '¡': '--...-', # Left ctrl?
+    'ch': '----', # Space?
+    'shift': '------', # Shift?
+    'err': '......', # Backspace?
+    'esc': '.......', # Esc?
+}
 
 MORSE_TO_CHAR = dict([(value, key) for key, value in MORSE.items()])
 
-# Use this controller to trigger actual keyboard key presses
-controller = keyboard.Controller()
+CURRENT_MODE = 'insert'  # or 'command'
 
-# Current sequency of dits & dahs.
-# When the timer is up, this is the letter that is interpreted.
+LEFT_ALT_DOWN = False
+LEFT_CTRL_DOWN = False
+LEFT_SHIFT_DOWN = False
+
+
+# The dits and dahs for the character that the user is currently trying to type:
 current = []
 
-# dit and dah timer used to make sure we don't send too many and get crazy
+# The characters that the user has successfully typed:
+text = []
+
 dit_timer = None
 dah_timer = None
 
-# TODO(DAN): use an actual event loop with predictable timing
 speed = 0.5
 
-# TODO(DAN): find better sinewave tone generator, use variable pitch e.g. 600Hz
-dit_sine = SineWave(pitch=12)
-dah_sine = SineWave(pitch=12)
+
+# TODO(DAN): keep trick of what was said, toggle SHIFT
+#dit_sine = SineWave(pitch=12)
+#dah_sine = SineWave(pitch=12)
 
 def end_char():
     global current
     global controller
+    global LEFT_ALT_DOWN
+    global LEFT_CTRL_DOWN
+    global LEFT_SHIFT_DOWN
     if current:
         # TODO(DAN): interpret dit/dahs as morse code key, make key press
         char = MORSE_TO_CHAR.get(''.join(current))
+        
         if char:
             char_lower = char.lower()
-            
-            # TODO(DAN): use map for special chars
-            if char_lower == 'esc':
+            if char_lower == 'err':
+                char_lower = keyboard.Key.backspace
+                controller.press(char_lower)
+                controller.release(char_lower)
+                print(char_lower)
+                if len(text):
+                    text.pop()
+            elif char_lower == 'esc':
                 char_lower = keyboard.Key.esc
-            elif char_lower == '=':
+                controller.press(char_lower)
+                controller.release(char_lower)
+                print(char_lower)
+            elif char_lower == 'ch':
+                char_lower = keyboard.Key.space
+                controller.press(char_lower)
+                controller.release(char_lower)
+                text.append(char_lower)
+                print(char_lower)
+            elif char_lower == '¿':
+                char_lower = keyboard.Key.alt_l
+                if LEFT_ALT_DOWN:
+                    controller.release(char_lower)
+                    LEFT_ALT_DOWN = False
+                else: 
+                    controller.press(char_lower)
+                    LEFT_ALT_DOWN = True
+            elif char_lower == 'shift':
+                char_lower = keyboard.Key.shift_l
+                if LEFT_SHIFT_DOWN:
+                    controller.release(char_lower)
+                    LEFT_SHIFT_DOWN = False
+                else: 
+                    controller.press(char_lower)
+                    LEFT_SHIFT_DOWN = True
+            elif char_lower == '¡':
+                char_lower = keyboard.Key.ctrl_l
+                if LEFT_CTRL_DOWN:
+                    controller.release(char_lower)
+                    LEFT_CTRL_DOWN = False
+                else: 
+                    controller.press(char_lower)
+                    LEFT_CTRL_DOWN = True
+            elif char_lower == '=' and LEFT_CTRL_DOWN:
                 char_lower = keyboard.Key.enter
-                
-            print(char_lower)
-            controller.press(char_lower)
-            controller.release(char_lower)
+                controller.press(char_lower)
+                controller.release(char_lower)
+                LEFT_CTRL_DOWN = False
+            else:
+                controller.press(char_lower)
+                controller.release(char_lower)
+                if LEFT_CTRL_DOWN:
+                    controller.release(keyboard.Key.ctrl_l)
+                    LEFT_CTRL_DOWN = False
+                elif LEFT_ALT_DOWN:
+                    controller.release(keyboard.Key.alt_l)
+                    LEFT_ALT_DOWN = False
+                elif LEFT_SHIFT_DOWN:
+                    controller.release(keyboard.Key.shift_l)
+                    LEFT_SHIFT_DOWN = False
+                else:
+                    print(char_lower)
+                    text.append(char_lower)
+
     current = []
 
-# Global for keeping track of when a letter has been completed
 char_timer = threading.Timer(1.0, end_char) 
 
-# Kill the dit sine wave
 def dit_complete():
     global dit_sine
+    #print('dit complete')
     #dit_sine.stop()
 
-# Kill the dah sine wave
 def dah_complete():
     global sinewave
+    #print('dah complete')
     #dah_sine.stop()
 
-    
 def handle_dit():
     global dit_sine
     global speed
@@ -123,8 +198,9 @@ def handle_dit():
         #dit_sine.stop()
         #dit_sine.play()
         print('dit');
-        dit_timer = threading.Timer(0.11 * speed, dit_complete) # TODO(DAN): timer should be .1 * speed
+        dit_timer = threading.Timer(0.11 * speed, dit_complete) 
         current.append('.')
+        print(current)
         dit_timer.start()
 
 def handle_dah():
@@ -137,16 +213,10 @@ def handle_dah():
         #dah_sine.stop()
         #dah_sine.play()
         print('dah');
-        dah_timer = threading.Timer(0.18 * speed, dah_complete) # TODO(DAN): timer should be .3 * speed
+        dah_timer = threading.Timer(0.18 * speed, dah_complete) 
         current.append('-')
         dah_timer.start()
 
-        
-# TODO(DAN): this should not be what triggers the actual sounds,
-# rather changes modifiers that an event loop (TODO) picks up.
-# We use Left-Control for dit and Right-Control for dah
-# This is to match the VBand USB Paddle adapter protocol:
-# https://hamradio.solutions/vband/ (see store $25 USD)
 def on_press(key):
     global speed
     global char_timer
@@ -166,12 +236,10 @@ def on_press(key):
         char_timer.start()
 
 
-# TODO(DAN): on release should affect modifiers in the event loop
-# TODO(DAN): come up with an application pause or quit routine based on SK prosigns or something cool
 def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
-        # exit() #TODO(DAN): quit strategy
+        #exit() #TODO(DAN): quit strategy
         pass
 
 
